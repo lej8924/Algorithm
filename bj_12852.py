a993f26a03be6dffeb33faf32dbd2f5b5c50437e
lej8924 <eunjae8924@naver.com>
2024-04-17 13:42:56 +0900

    solve 9184 by using dp

--- bj_12852.py
+++ bj_12852.py
@@ -7,11 +7,11 @@ for i in range(2,n+1):
     dp[i] = dp[i-1] + 1
     history[i] = i - 1
     
-    if i % 2 == 0:
-        dp[i] = min(dp[i],dp[i//2]+1)
+    if i % 2 == 0 and dp[i] > dp[i//2] + 1:
+        dp[i] = dp[i//2] + 1
         history[i] = i//2
-    if i % 3 ==0:
-        dp[i] = min(dp[i],dp[i//3]+1)
+    if i % 3 ==0 and dp[i] > dp[i//3] + 1:
+        dp[i] = dp[i//3] + 1
         history[i] = i//3
 
 print(dp[n])
